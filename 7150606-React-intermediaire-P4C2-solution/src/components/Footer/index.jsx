1d7f44aab81485ed14d43804aa1d96081c5cfdaa
git504 <git504@mailo.com>
2021-08-31 10:58:20 +0200

    first commit

---
+++ 7150606-React-intermediaire-P4C2-solution/src/components/Footer/index.jsx
@@ -0,0 +1,35 @@
+import { useTheme } from '../../utils/hooks'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import EmailInput from '../EmailInput'
+
+const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 60px 0;
+`
+
+const NightModeButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  color: ${colors.secondary};
+  padding-top: 30px;
+`
+
+function Footer() {
+  const { toggleTheme, theme } = useTheme()
+
+  return (
+    <FooterContainer>
+      <EmailInput theme={theme} />
+      <NightModeButton onClick={() => toggleTheme()}>
+        Changer en mode {theme === 'light' ? 'dark 🌙' : 'light ☀️'}
+      </NightModeButton>
+    </FooterContainer>
+  )
+}
+
+export default Footer
